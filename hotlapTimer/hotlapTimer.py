67c5995416e518e882adadb5a9232aa1b3352cec
Seth Morenos <sethvm64@gmail.com>
2020-07-04 03:47:45 -0400

    extend widget top padding

--- hotlapTimer/hotlapTimer.py
+++ hotlapTimer/hotlapTimer.py
@@ -43,58 +43,58 @@ def acMain(ac_version):
     global best_lap_label, best_lap, best_split_1, best_split_2, best_split_3
 
     # render widget
-    appWindow = ac.newApp("Hotlap Watch")
-    ac.setSize(appWindow, 420, 160)
+    appWindow = ac.newApp("Hotlap Timer")
+    ac.setSize(appWindow, 420, 165)
 
-    ac.console("Hotlap Watch a0.01 - loaded")
-    ac.log("Hotlap Watch a0.01 - loaded")
+    ac.console("Hotlap Timer a0.01 - loaded")
+    ac.log("Hotlap Timer a0.01 - loaded")
 
     # column headers (split column spacing: 80px)
     lap_header = ac.addLabel(appWindow, "Lap")
-    ac.setPosition(lap_header, 75, 31)
+    ac.setPosition(lap_header, 75, 36)
     split1_header = ac.addLabel(appWindow, "Split 1")
-    ac.setPosition(split1_header, 175, 31)
+    ac.setPosition(split1_header, 175, 36)
     split2_header = ac.addLabel(appWindow, "Split 2")
-    ac.setPosition(split2_header, 255, 31)
+    ac.setPosition(split2_header, 255, 36)
     split3_header = ac.addLabel(appWindow, "Split 3")
-    ac.setPosition(split3_header, 335, 31)
+    ac.setPosition(split3_header, 335, 36)
 
     # row labels (spacing: 30px)
     curr_lap_label = ac.addLabel(appWindow, "Curr")
-    ac.setPosition(curr_lap_label, 14, 62)
+    ac.setPosition(curr_lap_label, 14, 67)
     prev_lap_label = ac.addLabel(appWindow, "Prev")
-    ac.setPosition(prev_lap_label, 14, 92)
+    ac.setPosition(prev_lap_label, 14, 97)
     best_lap_label = ac.addLabel(appWindow, "Best")
-    ac.setPosition(best_lap_label, 14, 122)
+    ac.setPosition(best_lap_label, 14, 127)
 
     # current lap row
     curr_lap = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(curr_lap, 75, 62)
+    ac.setPosition(curr_lap, 75, 67)
     curr_split_1 = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(curr_split_1, 175, 62)
+    ac.setPosition(curr_split_1, 175, 67)
     curr_split_2 = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(curr_split_2, 255, 62)
+    ac.setPosition(curr_split_2, 255, 67)
     curr_split_3 = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(curr_split_3, 335, 62)
+    ac.setPosition(curr_split_3, 335, 67)
 
     # previous lap row
     prev_lap = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(prev_lap, 75, 92)
+    ac.setPosition(prev_lap, 75, 97)
     prev_split_1 = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(prev_split_1, 175, 92)
+    ac.setPosition(prev_split_1, 175, 97)
     prev_split_2 = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(prev_split_2, 255, 92)
+    ac.setPosition(prev_split_2, 255, 97)
     prev_split_3 = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(prev_split_3, 335, 92)
+    ac.setPosition(prev_split_3, 335, 97)
 
     # best lap row
     best_lap = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(best_lap, 75, 122)
+    ac.setPosition(best_lap, 75, 127)
     best_split_1 = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(best_split_1, 175, 122)
+    ac.setPosition(best_split_1, 175, 127)
     best_split_2 = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(best_split_2, 255, 122)
+    ac.setPosition(best_split_2, 255, 127)
     best_split_3 = ac.addLabel(appWindow, "--:--.---")
-    ac.setPosition(best_split_3, 335, 122)
+    ac.setPosition(best_split_3, 335, 127)
 
-    return "Hotlap Watch"
+    return "Hotlap Timer"
